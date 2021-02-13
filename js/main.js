4ca3b4a7bbba6529774c0de9e79dd087ab60feb5
erlynascarate <erlyndavid@outlook.es>
2021-02-13 12:39:08 -0500

    cuando el menu que se abre esta abierto y precionan a otro lado, se cierra

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 let menuToggle = document.querySelector('.menu-toggle');
 let menuToggleIcon = document.querySelector('.menu-toggle i');
 let menu = document.getElementById('menu');
+let menuLink1 = document.querySelector('.link1');
+let menuLink2 = document.querySelector('.link2');
+let menuLink3 = document.querySelector('.link3');
+let menuLink4 = document.querySelector('.link4');
 
 menuToggle.addEventListener('click', e=>{
    menu.classList.toggle('show');
@@ -8,5 +12,11 @@ menuToggle.addEventListener('click', e=>{
       menuToggleIcon.setAttribute('class', 'fa fa-times');
    }else{
       menuToggleIcon.setAttribute('class', 'fa fa-bars');
+   }
+})
+
+window.addEventListener('click', e=>{
+   if (menu.classList.contains('show') && e.target != menuLink1 && e.target != menuLink2 && e.target != menuLink3 && e.target != menuLink4 && e.target != menuToggle){
+      menu.classList.toggle('show');
    }
 })
